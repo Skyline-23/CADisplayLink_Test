3750ad22830c886dcdf551a3bc845b4c075a83da
Skyline-23 <bs2740@naver.com>
2022-05-24 14:30:27 +0900

    [UPDATE] to print displayLinkTest

--- CADisplayLink_Test/ViewController.swift
+++ CADisplayLink_Test/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    createDisplayLink()
     // Do any additional setup after loading the view.
   }
-
+  
+  func createDisplayLink() {
+    let displayLink: CADisplayLink = CADisplayLink.init(target: self, selector: #selector(stamp(displaylink:)))
+    // 주사율 제한
+    displayLink.preferredFrameRateRange = CAFrameRateRange.init(minimum:8, maximum:15, preferred:0)
+    displayLink.add(to: .main, forMode: RunLoop.Mode.default)
+  }
+  
+  @objc func stamp(displaylink: CADisplayLink) {
+    print("\(displaylink.timestamp) displayUpdated \(displaylink.duration)")
+    let actualFramesPerSecond = 1 / (displaylink.targetTimestamp - displaylink.timestamp)
+    print("actualFrame \(actualFramesPerSecond)")
+  }
 
 }
 
